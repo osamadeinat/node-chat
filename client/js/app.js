ee4ed90add3697cf2d2ca2043c3a6379428f8b16
othenat <osama.denat@gmail.com>
2016-01-07 23:33:51 +0000

    New-FEAT : is typing

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,6 @@
 
 var angular = this.angular;
 var io = this.io;
-console.log(angular);
 var app = angular.module('app', []);
 
 app.controller('ChatController', function($scope) {
@@ -12,6 +11,8 @@ app.controller('ChatController', function($scope) {
         $scope.roster = [];
         $scope.name = '';
         $scope.text = '';
+        $scope.typingMsg = false;
+        $scope.userTyping = '';
 
         socket.on('connect', function () {
           $scope.setName();
@@ -21,11 +22,24 @@ app.controller('ChatController', function($scope) {
           $scope.messages.push(msg);
           $scope.$apply();
         });
+        
+        socket.on('usr-typing', function(username) {
+           $scope.typingMsg = (username != '');
+           $scope.userTyping = (username.length > 0) ? username : "";
+           $scope.$apply();
+        });
 
         socket.on('roster', function (names) {
           $scope.roster = names;
           $scope.$apply();
         });
+        
+        $scope.typing = function typing() {
+          var userTypeNow = ($scope.text != '' && $scope.name != '') ? $scope.name : 'Guest';
+          var isTyping = ($scope.text != '');
+          console.info('typing now .. !');
+          socket.emit('typing', [userTypeNow, isTyping]);
+        }
 
         $scope.send = function send() {
           console.log('Sending message:', $scope.text);
